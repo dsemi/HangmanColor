668c4e76f615b5a1395677b195b1434ede501b98
Kenny Ruddick <ruddick1@tcnj.edu>
2013-03-17 03:15:39 -0400

    Added preliminary guess input

--- client/HMColorClient/main.js
+++ client/HMColorClient/main.js
@@ -1,27 +1,47 @@
-var puzzle, colorPanel;
+var puzzle, colorPanel, imgpath;
 
-var a = new AJAX('puzzle.py/');
 puzzle = new Puzzle('puzzle');
 
-a.onSuccess = function(response) {
-	puzzle.setImage(response, function() {
+var pRequest = new AJAX('puzzle.py/');
+pRequest.onSuccess = function(response) {
+	imgpath = response;
+	puzzle.setImage(imgpath, function() {
 		puzzle.draw();
 	});
 }
 
-a.onFail = function() {
+pRequest.onFail = function() {
 	puzzle.setImage('images/blackbear.jpg', function() {
 		puzzle.draw();
 	});
 }
 
-a.send('GET', 'puzzle_gen');
+pRequest.send('GET', 'puzzle_gen');
 
 colorPanel = new ColorPanel('color_panel', function(evt) {
 	var index, color;
 	index = evt.target.getAttribute('index');
 	color = colorPanel.colors[index];
-	
+
 	puzzle.addColor(color);
 	puzzle.draw();
 });
+
+document.getElementById('guess_submit').addEventListener('click', function() {
+	var guess, submit;
+	guess = document.getElementById('guess_input').value;
+	submit = new AJAX('puzzle.py/');
+	submit.onSuccess = function(response) {
+		alert(response);
+	};
+
+	submit.onFail = function() {
+		alert(guess);
+	}
+
+	submit.send('GET', 'soln', {
+		guess : guess,
+		filepath : imgpath
+	});
+});
+
