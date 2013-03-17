89a946e8a1cedc79162dcaf1ccf7d4da30683251
Kenny Ruddick <ruddick1@tcnj.edu>
2013-03-17 03:42:12 -0400

    More styling

--- client/HMColorClient/main.js
+++ client/HMColorClient/main.js
@@ -1,22 +1,22 @@
 var puzzle, colorPanel, imgpath;
 
-puzzle = new Puzzle('puzzle');
+puzzle = new Puzzle('puzzle_canvas');
 
-var pRequest = new AJAX('puzzle.py/');
-pRequest.onSuccess = function(response) {
+var puzzleRequest = new AJAX('puzzle.py/');
+puzzleRequest.onSuccess = function(response) {
 	imgpath = response;
 	puzzle.setImage(imgpath, function() {
 		puzzle.draw();
 	});
 }
 
-pRequest.onFail = function() {
+puzzleRequest.onFail = function() {
 	puzzle.setImage('images/blackbear.jpg', function() {
 		puzzle.draw();
 	});
 }
 
-pRequest.send('GET', 'puzzle_gen');
+puzzleRequest.send('GET', 'puzzle_gen');
 
 colorPanel = new ColorPanel('color_panel', function(evt) {
 	var index, color;
@@ -31,13 +31,10 @@ document.getElementById('guess_submit').addEventListener('click', function() {
 	var guess, submit;
 	guess = document.getElementById('guess_input').value;
 	submit = new AJAX('puzzle.py/');
+	
 	submit.onSuccess = function(response) {
 		alert(response);
 	};
-
-	submit.onFail = function() {
-		alert(guess);
-	}
 
 	submit.send('GET', 'soln', {
 		guess : guess,
