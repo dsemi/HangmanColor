416c85cf2b27b09f15855353a3f54b8c6e049289
Kenny Ruddick <ruddick1@tcnj.edu>
2013-03-16 22:47:32 -0400

    Added listener to the button to test AJAX

--- client/HMColorClient/main.js
+++ client/HMColorClient/main.js
@@ -11,12 +11,16 @@ puzzle.setImage(img_url, function() {
 
 document.getElementById('add_button').addEventListener('click', function() {
 	puzzle.addColor({
-		red: 0,
-		blue: 0,
-		green: 0
+		red : 0,
+		blue : 0,
+		green : 0
 	});
 	puzzle.draw();
+
+	var a = new AJAX('http://li244-77.members.linode.com/puzzle.py/');
+	a.get('puzzle_gen', null, function() {
+		alert('here');
+	});
+
 });
-
-
 
